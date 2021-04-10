7f84cd24240c5b667a0db0092fa5da9a44799152
yagneshl <55007619+yagneshl@users.noreply.github.com>
2021-04-10 01:53:42 -0400

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const skipToMainContentOnClick = () => {
     const mainContentNode = document.getElementById("main-content");
     mainContentNode.focus();
@@ -11,3 +12,18 @@ const addCopyrightText = () => {
         copyrightTextNode.textContent = `\u00A9 ${today.getFullYear()} Yagnesh Ladumor`;
     }
 }
+=======
+const skipToMainContentOnClick = () => {
+    const mainContentNode = document.getElementById("main-content");
+    mainContentNode.focus();
+}
+
+const addCopyrightText = () => {
+    const copyrightTextNodes = document.getElementsByClassName("copyrightText");
+    if (copyrightTextNodes.length > 0) {
+        const copyrightTextNode = copyrightTextNodes[0];
+        const today = new Date();
+        copyrightTextNode.textContent = `\u00A9 ${today.getFullYear()} Yagnesh Ladumor`;
+    }
+}
+>>>>>>> Stashed changes
